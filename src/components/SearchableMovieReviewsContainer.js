8a82af6ef00deedb5a801a08ca77c49a505ddbe7
George <georuiz817@gmail.com>
2019-10-27 19:10:51 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,51 @@
+//<SearchableMovieReviewsContainer />
+//should have state:
+//should have the state properties "reviews" and "searchTerm"
+
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
-
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+import MovieReviews from '/Users/dawn/react-container-components-lab-online-web-ft-061019/src/components/MovieReviews.js';
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor(props){
+        super(props)
+//should have state:
+//should have the state properties "reviews" and "searchTerm"
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+
+
+    handleInputChange = e => {
+        this.setState({ searchTerm: e.target.value})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+
+        fetch('https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=<search-term>'.concat(this.state.searchTerm))
+            .then(response => response.json())
+            .then(res => this.setState({reviews: res.result}))
+    }
+  
+      render(){
+            return(
+                <div className='searchable-movie-reviews'>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor='search-input'>Search Reviews   </label>
+                        <input id='search-input' type='text' onChange={this.handleInputChange} />
+                        <button type='submit'>Search</button>
+                    </form>
+                    {typeof this.state.reviews === 'object' &&
+                        this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
+                    <MovieReviews reviews={this.state.reviews} />
+                </div>
+            )
+        }
+
+
+
+}
